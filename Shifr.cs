26710f25987cc8169e13cc4143bf62632d2c298a
PashaRori <52232626+PashaRori@users.noreply.github.com>
2019-07-02 12:39:39 +0300

    Add files via upload

---
+++ Shifr.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp12Design
+{
+    class Shifr
+    {
+        string password = "P11a12v13e14l";
+        string salt = "K15n16y17s18h";
+        string hashAlgorithm = "MD5";
+        int passwordIterations = 16;
+        string initialVector = "meetstudendUST-42";
+        int keySize = 256;
+
+        public string Encrypt(string plainText)
+        {
+            if (string.IsNullOrEmpty(plainText))
+                return "";
+            byte[] initialVectorBytes = Encoding.ASCII.GetBytes(initialVector);
+            byte[] saltValueBytes = Encoding.ASCII.GetBytes(salt);
+            byte[] plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+            PasswordDeriveBytes derivedPassword = new PasswordDeriveBytes(password, saltValueBytes, hashAlgorithm, passwordIterations);
+            byte[] keyBytes = derivedPassword.GetBytes(keySize / 8);
+            RijndaelManaged symmetricKey = new RijndaelManaged();
+            symmetricKey.Mode = CipherMode.CBC;
+            byte[] cipherTextBytes = null;
+            using (ICryptoTransform encryptor = symmetricKey.CreateEncryptor(keyBytes, initialVectorBytes))
+            {
+                using (MemoryStream memStream = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream(memStream, encryptor, CryptoStreamMode.Write))
+                    {
+                        cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
+                        cryptoStream.FlushFinalBlock();
+                        cipherTextBytes = memStream.ToArray();
+                        memStream.Close();
+                        cryptoStream.Close();
+                    }
+                }
+            }
+            symmetricKey.Clear();
+            return Convert.ToBase64String(cipherTextBytes);
+        }
+
+
+        public string Decrypt(string cipherText)
+        {
+            if (string.IsNullOrEmpty(cipherText))
+                return "";
+            byte[] initialVectorBytes = Encoding.ASCII.GetBytes(initialVector);
+            byte[] saltValueBytes = Encoding.ASCII.GetBytes(salt);
+            byte[] cipherTextBytes = Convert.FromBase64String(cipherText);
+            PasswordDeriveBytes derivedPassword = new PasswordDeriveBytes(password, saltValueBytes, hashAlgorithm, passwordIterations);
+            byte[] keyBytes = derivedPassword.GetBytes(keySize / 8);
+            RijndaelManaged symmetricKey = new RijndaelManaged();
+            symmetricKey.Mode = CipherMode.CBC;
+            byte[] plainTextBytes = new byte[cipherTextBytes.Length];
+            int byteCount = 0;
+            using (ICryptoTransform decryptor = symmetricKey.CreateDecryptor(keyBytes, initialVectorBytes))
+            {
+                using (MemoryStream memStream = new MemoryStream(cipherTextBytes))
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream(memStream, decryptor, CryptoStreamMode.Read))
+                    {
+                        byteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
+                        memStream.Close();
+                        cryptoStream.Close();
+                    }
+                }
+            }
+            symmetricKey.Clear();
+            return Encoding.UTF8.GetString(plainTextBytes, 0, byteCount);
+        }
+    }
+}
